5cbd9d558a3fe39310299391c98fce9685b3b476
Shuky Chen <shukyc19@gmail.com>
2013-12-09 01:39:18 +0200

    Add ruby version check on sublime_debug_require

--- sublime_debug_require.rb
+++ sublime_debug_require.rb
@@ -1,10 +1,22 @@
-begin
-  require 'byebug'
-rescue LoadError
-  puts "Byebug gem is not installed for this ruby version: #{RUBY_VERSION}"
-  puts "please run 'gem install debugger' while using your default ruby version"
-  exit
+if RUBY_VERSION == "1.9.3"
+  begin
+    require 'debugger'
+  rescue LoadError
+    puts "Debugger gem is not installed for this ruby version: #{RUBY_VERSION}"
+    puts "please run 'gem install debugger' while using your default ruby version"
+    exit
+  end
+elsif RUBY_VERSION == "2.0.0"
+  begin
+    require 'byebug'
+  rescue LoadError
+    puts "Byebug gem is not installed for this ruby version: #{RUBY_VERSION}"
+    puts "please run 'gem install byebug' while using your default ruby version"
+    exit
+  end
 end
+
+
 
 begin
   Byebug.wait_connection = true
